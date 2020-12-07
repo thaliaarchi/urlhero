7e1ccca7654891df4a2bea9ebf56d7f7531dd9c2
Andrew Archibald <andrew@aarchibald.com>
2020-12-07 09:31:51 -0800

    Split function to prevent defer stacking

--- process/process.go
+++ process/process.go
@@ -23,93 +23,89 @@ func main() {
 }
 
 func processAll(root string) error {
-	contents, err := ioutil.ReadDir(root)
+	rootContents, err := ioutil.ReadDir(root)
 	if err != nil {
 		return err
 	}
-	for _, fi := range contents {
-		if !fi.IsDir() {
+	for _, release := range rootContents {
+		if !release.IsDir() {
 			continue
 		}
-		if err := process(filepath.Join(root, fi.Name())); err != nil {
+		dir := filepath.Join(root, release.Name())
+		dirContents, err := ioutil.ReadDir(dir)
+		if err != nil {
 			return err
+		}
+		for _, file := range dirContents {
+			name := file.Name()
+			if !strings.HasSuffix(name, ".zip") {
+				continue
+			}
+			if _, err := processZip(filepath.Join(dir, name), file.Size()); err != nil {
+				return err
+			}
 		}
 	}
 	return nil
 }
 
-func process(dirname string) error {
-	dir, err := os.Open(dirname)
+func processZip(filename string, size int64) (*Meta, error) {
+	f, err := os.Open(filename)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	defer dir.Close()
-	contents, err := dir.Readdir(-1)
-	if err != nil {
-		return err
-	}
+	defer f.Close()
 
-	for _, fi := range contents {
-		name := fi.Name()
-		fmt.Println(filepath.Join(filepath.Base(dirname), name))
-		if !strings.HasSuffix(name, ".zip") {
+	r, err := zip.NewReader(f, size)
+	if err != nil {
+		return nil, err
+	}
+	var meta *Meta
+	for _, zf := range r.File {
+		fmt.Printf("%s\n", filepath.Join(filename, zf.Name))
+		if !strings.HasSuffix(zf.Name, ".xz") || zf.FileInfo().IsDir() {
 			continue
 		}
-		f, err := os.Open(filepath.Join(dirname, name))
-		if err != nil {
-			return err
-		}
-		defer f.Close()
 
-		f.Stat()
-		r, err := zip.NewReader(f, fi.Size())
+		zr, err := zf.Open()
 		if err != nil {
-			return err
+			return nil, err
 		}
-		var meta Meta
-		for _, zf := range r.File {
-			fmt.Println(" ", zf.Name)
-			if !strings.HasSuffix(zf.Name, ".xz") || zf.FileInfo().IsDir() {
-				continue
-			}
-			zr, err := zf.Open()
-			if err != nil {
-				return err
-			}
-			xr, err := xz.NewReader(zr)
-			if err != nil {
-				return err
-			}
+		defer zr.Close()
+		xr, err := xz.NewReader(zr)
+		if err != nil {
+			return nil, err
+		}
 
-			switch {
-			case strings.HasSuffix(zf.Name, ".meta.json.xz"):
-				jd := json.NewDecoder(xr)
-				jd.DisallowUnknownFields()
-				if err := jd.Decode(&meta); err != nil {
-					return err
+		switch {
+		case strings.HasSuffix(zf.Name, ".meta.json.xz"):
+			jd := json.NewDecoder(xr)
+			jd.DisallowUnknownFields()
+			var m Meta
+			if err := jd.Decode(&m); err != nil {
+				return nil, err
+			}
+			meta = &m
+			continue
+		case strings.HasSuffix(zf.Name, ".txt.xz"):
+			br := beacon.NewReader(xr)
+			_, err := br.Header()
+			if err != nil {
+				return nil, err
+			}
+			for {
+				_, err := br.Read()
+				if err == io.EOF {
+					break
 				}
-				continue
-			case strings.HasSuffix(zf.Name, ".txt.xz"):
-				br := beacon.NewReader(xr)
-				header, err := br.Header()
 				if err != nil {
-					return err
-				}
-				_ = header
-				for {
-					link, err := br.Read()
-					if err == io.EOF {
-						break
-					}
-					if err != nil {
-						fmt.Fprintln(os.Stderr, filepath.Join(dirname, name, zf.Name), err)
-					}
-					_ = link
+					fmt.Fprintln(os.Stderr, filepath.Join(filepath.Base(filename), zf.Name), err)
+					continue
 				}
 			}
 		}
 	}
-	return nil
+	return meta, nil
 }
 
 type Meta struct {
