7e9a9ec657de24b7d544199cc792d9ba792ceead
Andrew Archibald <andrew@aarchibald.com>
2021-04-01 03:56:58 -0600

    Decode base32-encoded SHA-1 digests

--- ia/timemap.go
+++ ia/timemap.go
@@ -59,3 +59,39 @@ func GetTimemap(pageURL string, options *TimemapOptions) ([][]string, error) {
 	}
 	return timemap, nil
 }
+
+// DecodeDigest decodes a base32-encoded SHA-1 digest.
+func DecodeDigest(digest string) (*[20]byte, error) {
+	if len(digest) != 32 {
+		return nil, fmt.Errorf("ia: digest not 32 bytes: %q", digest)
+	}
+	var b [20]byte
+	bit := 0
+	for i := 0; i < 32; i++ {
+		x, err := digestAlpha(digest[i], digest)
+		if err != nil {
+			return nil, err
+		}
+		// Fill 5-bit digits into 8-bit bytes:
+		x <<= 3
+		b[bit/8] |= x >> (bit % 8)
+		if bit/8 < 19 {
+			b[bit/8+1] |= x << (8 - bit%8)
+		}
+		bit += 5
+	}
+	return &b, nil
+}
+
+// digestAlpha returns the base-32 value for the character in the
+// alphabet "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567".
+func digestAlpha(ch byte, digest string) (byte, error) {
+	switch {
+	case 'A' <= ch && ch <= 'Z':
+		return ch - 'A', nil
+	case '2' <= ch && ch <= '7':
+		return ch - '2' + 26, nil
+	default:
+		return 0, fmt.Errorf("ia: illegal byte %q in digest: %q", ch, digest)
+	}
+}
