b1e288d1f1815d15e01eb9a4f182f5d75806223e
Andrew Archibald <andrew@aarchibald.com>
2021-04-03 13:41:35 -0600

    Update license header years for files created in 2020

--- tinytown/walk.go
+++ tinytown/walk.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 Andrew Archibald
+// Copyright (c) 2020-2021 Andrew Archibald
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
