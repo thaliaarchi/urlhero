01280135874599ad2c2926db53753b260d6360e1
Andrew Archibald <andrew@aarchibald.com>
2021-04-03 19:28:18 -0600

    Export known list of bit.ly aliases

--- shorteners/bit-ly/alias.go
+++ shorteners/bit-ly/alias.go
@@ -4,12 +4,141 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+// Package bitly handles the bit.ly link shortener and its aliases.
 package bitly
 
 import (
 	"encoding/binary"
 	"net"
 )
+
+// Aliases is a list of the hostnames of known bit.ly aliases.
+// See https://wiki.archiveteam.org/index.php?title=URLTeam#bit.ly_aliases
+var Aliases = []string{
+	"0-60.in",
+	"030mag.de",
+	"1.azdhs.gov",
+	"20ss.nyc",
+	"360myte.ch",
+	"4sq.com",
+	"511t.ac",
+	"6.wate.com",
+	"7gee.se",
+	"8legs.co",
+	"9l7gf7x5o9v.xyz",
+	"imdb.to",
+	"abcn.ws",
+	"accntu.re",
+	"actf.tv",
+	"adobe.ly",
+	"adweek.it",
+	"amzn.to",
+	"apple.co",
+	"arfo.sk",
+	"audi.us",
+	"autism.link",
+	"azc.cc",
+	"bbc.in",
+	"beta.works",
+	"binged.it",
+	"bitly.is",
+	"blizz.ly",
+	"bloom.bg",
+	"bo.st",
+	"bravo.ly",
+	"bzfd.it",
+	"canva.link",
+	"cb.com",
+	"chfstps.co",
+	"chzb.gr",
+	"cnet.co",
+	"cnnmon.ie",
+	"comca.st",
+	"conta.cc",
+	"crks.me",
+	"d-mk.co",
+	"don8blood.com",
+	"econ.st",
+	"engri.sh",
+	"eonli.ne",
+	"es.pn",
+	"fltsim.me",
+	"fxn.ws",
+	"got.cr",
+	"hrkey.co",
+	"hub.am",
+	"huff.to",
+	"idle-empi.re",
+	"ift.tt",
+	"kck.st",
+	"kore.us",
+	"krg.bz",
+	"lat.ms",
+	"laurc.ro",
+	"lemde.fr",
+	"lft.to",
+	"m.ttmask.com",
+	"marsdd.it",
+	"mbist.ro",
+	"mojo.ly",
+	"mttr.io",
+	"nydn.us",
+	"nyti.ms",
+	"oculta.bit.ly",
+	"on.fb.me",
+	"on.natgeo.com",
+	"red.ht",
+	"reut.rs",
+	"sdut.us",
+	"snd.sc",
+	"spoti.fi",
+	"stanford.io",
+	"tcrn.ch",
+	"ti.me",
+	"usat.ly",
+	"wapo.st",
+	"wgrd.tech",
+	"wttw.me",
+	"xar.ph",
+	"yhoo.it",
+}
+
+/*
+Dead or problematic aliases from wiki list:
+
+DNS not in the range 67.199.248.10 to 67.199.248.13
+	1.usa.gov
+	aje.me
+	arcg.is
+	atfp.co
+	bbybgrl.com
+	bitly.com
+	bitlymail.com
+	corb.is
+	cot.ag
+	curbed.cc
+	gph.is
+	j.mp
+	lego.build
+	on.rare.us
+	on.si.com
+	qr.cm
+	theatln.tc
+	vstphl.ly
+	whrt.it
+	www.bitly.com
+	zmb.me
+
+Lookup: no such host
+	carrot.cr
+	emarketee.rs
+	grd.to
+	jrnl.to
+	stnfy.com
+
+Not a host
+	feedly.com/k/
+*/
 
 // IsAlias checks whether the domain is a bit.ly alias.
 func IsAlias(domain string) (bool, error) {
--- shorteners/bit-ly/alias_test.go
+++ shorteners/bit-ly/alias_test.go
@@ -9,143 +9,24 @@ package bitly
 import "testing"
 
 func TestIsAlias(t *testing.T) {
-	tests := []struct {
-		domain  string
-		isAlias bool
-	}{
-		// Known bit.ly aliases
-		// https://wiki.archiveteam.org/index.php?title=URLTeam#bit.ly_aliases
-		{"0-60.in", true},
-		{"030mag.de", true},
-		{"1.azdhs.gov", true},
-		{"20ss.nyc", true},
-		{"360myte.ch", true},
-		{"4sq.com", true},
-		{"511t.ac", true},
-		{"6.wate.com", true},
-		{"7gee.se", true},
-		{"8legs.co", true},
-		{"9l7gf7x5o9v.xyz", true},
-		{"IMDB.to", true},
-		{"abcn.ws", true},
-		{"accntu.re", true},
-		{"actf.tv", true},
-		{"adobe.ly", true},
-		{"adweek.it", true},
-		{"amzn.to", true},
-		{"apple.co", true},
-		{"arfo.sk", true},
-		{"audi.us", true},
-		{"autism.link", true},
-		{"azc.cc", true},
-		{"bbc.in", true},
-		{"beta.works", true},
-		{"binged.it", true},
-		{"bitly.is", true},
-		{"blizz.ly", true},
-		{"bloom.bg", true},
-		{"bo.st", true},
-		{"bravo.ly", true},
-		{"bzfd.it", true},
-		{"canva.link", true},
-		{"cb.com", true},
-		{"chfstps.co", true},
-		{"chzb.gr", true},
-		{"cnet.co", true},
-		{"cnnmon.ie", true},
-		{"comca.st", true},
-		{"conta.cc", true},
-		{"crks.me", true},
-		{"d-mk.co", true},
-		{"don8blood.com", true},
-		{"econ.st", true},
-		{"engri.sh", true},
-		{"eonli.ne", true},
-		{"es.pn", true},
-		{"fltsim.me", true},
-		{"fxn.ws", true},
-		{"got.cr", true},
-		{"hrkey.co", true},
-		{"hub.am", true},
-		{"huff.to", true},
-		{"idle-empi.re", true},
-		{"ift.tt", true},
-		{"kck.st", true},
-		{"kore.us", true},
-		{"krg.bz", true},
-		{"lat.ms", true},
-		{"laurc.ro", true},
-		{"lemde.fr", true},
-		{"lft.to", true},
-		{"m.ttmask.com", true},
-		{"marsdd.it", true},
-		{"mbist.ro", true},
-		{"mojo.ly", true},
-		{"mttr.io", true},
-		{"nydn.us", true},
-		{"nyti.ms", true},
-		{"oculta.bit.ly", true},
-		{"on.fb.me", true},
-		{"on.natgeo.com", true},
-		{"red.ht", true},
-		{"reut.rs", true},
-		{"sdut.us", true},
-		{"snd.sc", true},
-		{"spoti.fi", true},
-		{"stanford.io", true},
-		{"tcrn.ch", true},
-		{"ti.me", true},
-		{"usat.ly", true},
-		{"wapo.st", true},
-		{"wgrd.tech", true},
-		{"wttw.me", true},
-		{"xar.ph", true},
-		{"yhoo.it", true},
-
-		// Returns false
-		//   1.usa.gov
-		//   aje.me
-		//   arcg.is
-		//   atfp.co
-		//   bbybgrl.com
-		//   bitly.com
-		//   bitlymail.com
-		//   corb.is
-		//   cot.ag
-		//   curbed.cc
-		//   gph.is
-		//   j.mp
-		//   lego.build
-		//   on.rare.us
-		//   on.si.com
-		//   qr.cm
-		//   theatln.tc
-		//   vstphl.ly
-		//   whrt.it
-		//   www.bitly.com
-		//   zmb.me
-
-		// Lookup: no such host
-		//   carrot.cr
-		//   emarketee.rs
-		//   grd.to
-		//   jrnl.to
-		//   stnfy.com
-
-		// Not a host
-		//   feedly.com/k/
-
-		{"urlte.am", false},
-		{"wiki.archiveteam.org", false},
+	nonAliases := []string{
+		"urlte.am",
+		"wiki.archiveteam.org",
 	}
-	for _, test := range tests {
-		isAlias, err := IsAlias(test.domain)
-		if err != nil {
-			t.Errorf("IsAlias(%s): %v", test.domain, err)
-			continue
-		}
-		if isAlias != test.isAlias {
-			t.Errorf("IsAlias(%s) = %t, want %t", test.domain, isAlias, test.isAlias)
-		}
+	for _, host := range Aliases {
+		checkAlias(t, host, true)
+	}
+	for _, host := range nonAliases {
+		checkAlias(t, host, false)
+	}
+}
+
+func checkAlias(t *testing.T, host string, want bool) {
+	t.Helper()
+	isAlias, err := IsAlias(host)
+	if err != nil {
+		t.Errorf("IsAlias(%s): %v", host, err)
+	} else if isAlias != want {
+		t.Errorf("IsAlias(%s) = %t, want %t", host, isAlias, want)
 	}
 }
